95ab6e0049afc57b9b990b5b22bc1eb3befc474f
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2026-08-15 02:35:49 +0000

    Built site for gh-pages

--- post-packages.R
+++
@@ -1,5 +0,0 @@
-needed <- unique(renv::dependencies()$Package)
-
-needed <- needed[!(needed %in% installed.packages())]
-
-install.packages(needed, repos = 'https://packagemanager.posit.co/cran/latest')
